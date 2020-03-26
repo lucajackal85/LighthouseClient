daa5ae63d00073143a8243e2200bd874e29be4ea
Luca <luca.giacalone@edimotive.com>
2020-03-26 14:30:35 +0100

    +fix tests

--- tests/Process/LighthouseProcessTest.php
+++ tests/Process/LighthouseProcessTest.php
@@ -7,12 +7,16 @@ use PHPUnit\Framework\TestCase;
 
 class LighthouseProcessTest extends TestCase
 {
+    protected function getLightousePath(){
+        return trim(exec('which lighthouse'));
+    }
+
     public function testGetCommandStringDefaultValues(){
         $process = new LightHouseProcess([
             'path' => __DIR__ . '/unittest.json',
         ]);
 
-        $this->assertEquals('/usr/bin/lighthouse http://localhost/ --chrome-flags="--headless --no-sandbox" --output=json --output-path=' . __DIR__ . '/unittest.json',
+        $this->assertEquals($this->getLightousePath().' http://localhost/ --chrome-flags="--headless --no-sandbox" --output=json --output-path=' . __DIR__ . '/unittest.json',
             $process->getProcess('http://localhost/')->getCommandLine()
         );
     }
@@ -23,7 +27,7 @@ class LighthouseProcessTest extends TestCase
             'interactive' => true,
         ]);
 
-        $this->assertEquals('/usr/bin/lighthouse http://localhost/ --output=json --output-path=' . __DIR__ . '/unittest.json',
+        $this->assertEquals($this->getLightousePath().' http://localhost/ --output=json --output-path=' . __DIR__ . '/unittest.json',
             $process->getProcess('http://localhost/')->getCommandLine()
         );
     }
@@ -35,7 +39,7 @@ class LighthouseProcessTest extends TestCase
             'output' => 'html',
         ]);
 
-        $this->assertEquals('/usr/bin/lighthouse http://localhost/ --output=html --output-path=' . __DIR__ . '/unittest.html',
+        $this->assertEquals($this->getLightousePath().' http://localhost/ --output=html --output-path=' . __DIR__ . '/unittest.html',
             $process->getProcess('http://localhost/')->getCommandLine()
         );
     }
